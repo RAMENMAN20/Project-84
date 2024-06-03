0d507e91ed8904ad4d23081233c2cbcb6de21714
RAMENMAN20 <148730075+RAMENMAN20@users.noreply.github.com>
2024-06-02 17:52:49 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -33,6 +33,24 @@ function my_keydown(e)
 			document.getElementById("d1").innerHTML="You pressed Alphabet Key";
 			console.log("alphabet key");
 		}
+			else if (keyPressed >=48 && keyPressed<=57)
+			{
+				numberkey();
+				document.getElementById("d1").innerHTML="You pressed Number Key";
+				console.log("number key");
+			}
+				else if (keyPressed >=37 && keyPressed<=40)
+				{
+					arrowkey();
+					document.getElementById("d1").innerHTML="You pressed Arrow Key";
+					console.log("arrow key");
+				}
+					else if ((keyPressed ==17) ||( keyPressed ==18 || keyPressed ==27))
+					{
+						specialkey();
+						document.getElementById("d1").innerHTML="You pressed Ctrl/ Esc/ Alt Key";
+						console.log("special key");
+					}
 	else{
 		otherkey();
 		document.getElementById("d1").innerHTML="You pressed symbol or other key";
